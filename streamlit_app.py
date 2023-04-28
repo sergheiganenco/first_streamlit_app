915a472aea6b7796c51424720f13b83d57d851c0
sergheiganenco <sergheiganenco@users.noreply.github.com>
2023-04-28 12:45:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,13 +44,12 @@ try:
       streamlit.dataframe(back_from_function)
     
      #section to add the list     
-    streamlit.header("The fruit load list contains:")
+   streamlit.header("The fruit load list contains:")
     #Snowflake-related functions
-    def get_fruit_load_list():
-        with 
-            my_cnx,cursor() as my_cur:
-            my_cur.execute("Select * from fruit_load_list")
-            return my_cur.fetchall()
+   def get_fruit_load_list():
+       with my_cnx,cursor() as my_cur:
+           my_cur.execute("Select * from fruit_load_list")
+           return my_cur.fetchall()
 
 #Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
